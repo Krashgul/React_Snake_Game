f6bea14c9846d3a68c4a470d93e483c2b02b6228
ajdik <ajdik@o2.pl>
2021-02-23 02:51:18 +0100

    comit 01

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-
+import Snake from './Snake';
+import Food from './Food';
 
 
 class App extends Component {
+
+  state = {
+    food: [6,8],
+    snakeDots: [
+      [0, 0],
+      [2,0]
+  ]
+}
+
   render() {
     return (
       <div className="game-area">
-        <div className="snake-dot" style={{ top: 0, left: 0 }}></div>
-        <div className="snake-dot" style={{ top: 0, left: '2%' }}></div>
-        <div className="snake-dot" style={{ top: 0, left: '4%' }}></div>
-        
+        <Snake snakeDots={this.state.snakeDots}/>
       </div>
     );
   }

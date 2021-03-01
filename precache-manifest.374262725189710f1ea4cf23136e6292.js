9894aab08addb3ef3db1076a6c0cbae95eadba36
ajdik <ajdik@o2.pl>
2021-03-02 00:13:46 +0100

    Updates

---
+++ precache-manifest.374262725189710f1ea4cf23136e6292.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a991fa17014322caa575",
+    "url": "/React_Snake_Game/static/css/main.2602c1c5.chunk.css"
+  },
+  {
+    "revision": "a991fa17014322caa575",
+    "url": "/React_Snake_Game/static/js/main.63d239d9.chunk.js"
+  },
+  {
+    "revision": "62df9cb6dd2316245edc",
+    "url": "/React_Snake_Game/static/js/runtime~main.c047f0de.js"
+  },
+  {
+    "revision": "6bb2372fe7b7eca4994e",
+    "url": "/React_Snake_Game/static/js/2.369be766.chunk.js"
+  },
+  {
+    "revision": "9e45580403848ff9751f8e7736e1ca0d",
+    "url": "/React_Snake_Game/index.html"
+  }
+];
